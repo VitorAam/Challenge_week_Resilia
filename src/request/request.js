45bb1f9ef865c84ece5650a07b63f65c4703e9cb
Vitor <v.augusto.am@gmail.com>
2022-10-02 21:11:30 -0300

    Update request.js

--- src/request/request.js
+++ src/request/request.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 export const getEstudante = () => {
-  const request = axios.get('http://localhost:3000/').then((response)=> response.data)
+  const request = axios.get('http://localhost:3001/').then((response)=> response.data)
   return request
 }
 
 export const postEstudante = (obj) => {
-  const post  = axios.post('http://localhost:3000/', obj)
+  const post  = axios.post('http://localhost:3001/', obj)
   return post
 }
 
 export const deleteEstudante = async (id) => {
-  return await axios.delete(`http://localhost:3000/${id}`)
+  return await axios.delete(`http://localhost:3001/${id}`)
 }
